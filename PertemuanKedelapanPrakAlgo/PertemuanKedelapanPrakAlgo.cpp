90a308d98c6f5758c78955a28e5e6db3ddd2b03f
Yowandaradillaa <yowandaradilla13@gmail.com>
2024-04-26 09:41:56 +0700

    Memasukan if else

--- PertemuanKedelapanPrakAlgo/PertemuanKedelapanPrakAlgo.cpp
+++ PertemuanKedelapanPrakAlgo/PertemuanKedelapanPrakAlgo.cpp
@@ -89,6 +89,16 @@ bool listEmpty() {
 		return false;
 
 }
-void traverse() {
 
+void traverse() {
+	if (listEmpty()) {
+		cout << "List kosong" << endl;
+		system("pause");
+		system("cls");
+		return;
+	}
+	else {
+		Node* currentNode = START;
+	
+	}
 }
